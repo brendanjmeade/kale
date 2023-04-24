8e147d4ba4feed3d530bd2bb6ead35808b943636
Bane Sullivan <bane.sullivan@kitware.com>
2023-04-24 10:12:41 -0400

    Update add_bounds

--- kale/helpers.py
+++ kale/helpers.py
@@ -24,3 +24,22 @@ def add_contours(plotter, source, scalars, levels, **kwargs):
         edges, color=theme.CONTOUR_LINE_COLOR, line_width=theme.CONTOUR_LINE_WIDTH
     )
     return actor_c, actor_e
+
+
+def add_bounds(plotter, **kwargs):
+    # Requires https://github.com/pyvista/pyvista/pull/3977
+    args = dict(
+        grid='back',
+        location='outer',
+        ticks='both',
+        n_xlabels=2,
+        n_ylabels=2,
+        n_zlabels=2,
+        xtitle='Easting',
+        ytitle='Northing',
+        ztitle='Elevation',
+        fmt='%.2f',
+    )
+    args.update(kwargs)
+
+    plotter.show_bounds(**args)
