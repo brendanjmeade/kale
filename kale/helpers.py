173e7b3fb43dff0939ca9954f4aa9ab06372c27b
Bane Sullivan <bane.sullivan@kitware.com>
2023-04-24 10:31:11 -0400

    Formatting

--- kale/helpers.py
+++ kale/helpers.py
@@ -29,16 +29,16 @@ def add_contours(plotter, source, scalars, levels, **kwargs):
 def add_bounds(plotter, **kwargs):
     # Requires https://github.com/pyvista/pyvista/pull/3977
     args = dict(
-        grid='back',
-        location='outer',
-        ticks='both',
+        grid="back",
+        location="outer",
+        ticks="both",
         n_xlabels=2,
         n_ylabels=2,
         n_zlabels=2,
-        xtitle='Easting',
-        ytitle='Northing',
-        ztitle='Elevation',
-        fmt='%.2f',
+        xtitle="Easting",
+        ytitle="Northing",
+        ztitle="Elevation",
+        fmt="%.2f",
     )
     args.update(kwargs)
 
